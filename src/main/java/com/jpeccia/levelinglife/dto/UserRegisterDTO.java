e77d1994fecc5741864377567c8063c6c8ee25cc
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 17:35:55 -0300

    feat: create UserRegister DTO

---
+++ src/main/java/com/jpeccia/levelinglife/dto/UserRegisterDTO.java
@@ -0,0 +1,10 @@
+package com.jpeccia.levelinglife.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegisterDTO {
+    private String username;
+    private String email;
+    private String password;
+}
