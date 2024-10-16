376d36fa6db2ef55f518a94fb68f409f97b4ef59
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 17:20:48 -0300

    feat: create CustomUserDetailsService

---
+++ src/main/java/com/jpeccia/levelinglife/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.jpeccia.levelinglife.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.jpeccia.levelinglife.entity.User;
+import com.jpeccia.levelinglife.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());  
+}
+}
