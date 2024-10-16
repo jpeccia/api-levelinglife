f888c5abc4f5702a489f21c135126a46e804d6a9
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 16:51:05 -0300

    feat: create User Repository

---
+++ src/main/java/com/jpeccia/levelinglife/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.jpeccia.levelinglife.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jpeccia.levelinglife.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    
+}
