9d6b5f092dabe29b04d77b4fd5282b3dc6cc81c1
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 15:58:24 -0300

    first commit

---
+++ src/main/java/com/jpeccia/levelinglife/LevelinglifeApplication.java
@@ -0,0 +1,13 @@
+package com.jpeccia.levelinglife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelinglifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelinglifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpeccia/levelinglife/LevelinglifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpeccia.levelinglife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelinglifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
