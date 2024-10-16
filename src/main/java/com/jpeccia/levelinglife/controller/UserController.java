6dfd8c92d474988a0596283e504baa3581e087f8
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 18:03:26 -0300

    feat: post register/login routes in User Controller

--- src/main/java/com/jpeccia/levelinglife/controller/UserController.java
+++ src/main/java/com/jpeccia/levelinglife/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpeccia.levelinglife.entity.User;
@@ -18,9 +19,24 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User user) throws Exception {
-        User newUser = userService.createUser(user);
-        return ResponseEntity.ok(newUser);
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User newUser = userService.createUser(user);
+            return ResponseEntity.ok(newUser);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestParam String usernameOrEmail, @RequestParam String password) {
+        try {
+            User user = userService.loginUser(usernameOrEmail, password);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
