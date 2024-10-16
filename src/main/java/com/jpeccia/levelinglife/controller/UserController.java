0a47683bce281fcb4ffd1e4dba163394a3f75995
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 17:33:53 -0300

    feat: create UserController and add register post end point

---
+++ src/main/java/com/jpeccia/levelinglife/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.jpeccia.levelinglife.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jpeccia.levelinglife.entity.User;
+import com.jpeccia.levelinglife.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+}
