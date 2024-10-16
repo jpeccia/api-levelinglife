25cd744f903c49a23d2c18c76bbc18eb4b3c15f5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 16:57:00 -0300

    feat: create Token Service

---
+++ src/main/java/com/jpeccia/levelinglife/infra/security/TokenService.java
@@ -0,0 +1,11 @@
+package com.jpeccia.levelinglife.infra.security;
+
+public class TokenService {
+    
+    public String generateToken(User user){
+        try {
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
