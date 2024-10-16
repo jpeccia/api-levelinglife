d7ab851ddffd694231b0b63bac8274cb2db9eacb
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-16 16:32:12 -0300

    feat: create User Entity

---
+++ src/main/java/com/jpeccia/levelinglife/entity/User.java
@@ -0,0 +1,45 @@
+package com.jpeccia.levelinglife.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int xp = 0;
+
+    private int level = 1;
+
+    private String profilePicture;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<> quests = new ArrayList<>();
+    
+}
