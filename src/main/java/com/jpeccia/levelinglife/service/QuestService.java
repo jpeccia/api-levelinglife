993110dd723318dc6562d0a18fc0f27ab5a65a10
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-23 14:50:48 -0300

    feat: create QuestService

---
+++ src/main/java/com/jpeccia/levelinglife/service/QuestService.java
@@ -0,0 +1,37 @@
+package com.jpeccia.levelinglife.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpeccia.levelinglife.entity.Quest;
+import com.jpeccia.levelinglife.repository.QuestRepository;
+
+@Service
+public class QuestService {
+    
+    @Autowired
+    private QuestRepository questRepository;
+
+    public List<Quest> findAllQuestsByUserId(Long userId){
+        return questRepository.findByUserId(userId);
+    }
+
+    public Quest createQuest(Quest quest){
+        return questRepository.save(quest);
+    }
+
+    public void completeQuest(Long questId){
+        Quest quest = questRepository.findById(questId)
+        .orElseThrow(() -> new IllegalArgumentException("Quest not exist!"));
+
+        quest.setCompleted(true);
+        questRepository.save(quest);
+    }
+
+    public void deleteQuest(Long questId){
+        questRepository.deleteById(questId);
+    }
+
+}
