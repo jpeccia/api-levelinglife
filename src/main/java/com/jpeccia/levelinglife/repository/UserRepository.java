4a54f4d27674da485b4b7f7f519f07bee3db4a54
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-30 15:44:32 -0300

    feat: add findByLogin in UserRepository

--- src/main/java/com/jpeccia/levelinglife/repository/UserRepository.java
+++ src/main/java/com/jpeccia/levelinglife/repository/UserRepository.java
@@ -3,11 +3,13 @@ package com.jpeccia.levelinglife.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.jpeccia.levelinglife.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByLogin(String login);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     Optional<User> findByUsernameOrEmail(String username, String email);
