4ccdef52022b8efb35b0914f2f17f585080c9dc5
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-30 16:23:38 -0300

    feat: add endpoints in SecurityConfig

--- src/main/java/com/jpeccia/levelinglife/infra/security/SecurityConfig.java
+++ src/main/java/com/jpeccia/levelinglife/infra/security/SecurityConfig.java
@@ -26,6 +26,9 @@ public class SecurityConfig {
                .csrf(csrf -> csrf.disable())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authorizeHttpRequests(authorize -> authorize
+                                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                                        .requestMatchers(HttpMethod.POST, "/quests/add").permitAll()
                                         .requestMatchers(HttpMethod.DELETE, "/quests/{id}").hasRole("ADMIN")
                                         .anyRequest().authenticated())
                .build();
