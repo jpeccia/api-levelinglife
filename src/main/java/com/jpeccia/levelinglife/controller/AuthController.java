4867b7a496c8806a99e94dee68a15317e03fa23a
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-30 16:07:30 -0300

    feat: add login in AuthController

--- src/main/java/com/jpeccia/levelinglife/controller/AuthController.java
+++ src/main/java/com/jpeccia/levelinglife/controller/AuthController.java
@@ -1,17 +1,28 @@
 package com.jpeccia.levelinglife.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpeccia.levelinglife.dto.AuthDTO;
+
 @RestController
 @RequestMapping("auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
     
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthDTO data){
-        
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
     }
 }
