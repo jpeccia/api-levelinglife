e9f134a9c7df4ca1bfd276e47b357ebdb3d8499a
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-10-30 21:15:17 -0300

    feat: read all users in UserControlller

--- src/main/java/com/jpeccia/levelinglife/controller/UserController.java
+++ src/main/java/com/jpeccia/levelinglife/controller/UserController.java
@@ -1,25 +1,26 @@
 package com.jpeccia.levelinglife.controller;
+import java.util.List;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpeccia.levelinglife.entity.User;
+import com.jpeccia.levelinglife.repository.UserRepository;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    
+
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/")
-    public ResponseEntity<String> getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            return ResponseEntity.ok(authentication.getName());
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
