60f9f83c60a5b623bebf10cdc2de44658c69e5d4
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-04 18:18:20 -0300

    feat: create Friendship Repository

---
+++ src/main/java/com/jpeccia/levelinglife/repository/FriendshipRepository.java
@@ -0,0 +1,17 @@
+package com.jpeccia.levelinglife.repository;
+
+import com.jpeccia.levelinglife.entity.Friendship;
+import com.jpeccia.levelinglife.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+
+    // Retorna uma lista de amigos de um usuário
+    List<Friendship> findByUser(User user);
+
+    // Verifica se uma amizade já existe entre dois usuários
+    Optional<Friendship> findByUserAndFriend(User user, User friend);
+}
