d017256c4cc60e487ca19383dbe09e6b478f3daa
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-04 16:26:32 -0300

    feat: add view profile in UserController

--- src/main/java/com/jpeccia/levelinglife/controller/UserController.java
+++ src/main/java/com/jpeccia/levelinglife/controller/UserController.java
@@ -3,10 +3,13 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpeccia.levelinglife.dto.UserProfileDTO;
 import com.jpeccia.levelinglife.entity.User;
 import com.jpeccia.levelinglife.repository.UserRepository;
 
@@ -22,5 +25,21 @@ public class UserController {
     @GetMapping("/")
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserProfileDTO> getProfile() {
+        // Obter o usuário autenticado do contexto de segurança
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // Construir o UserProfileDTO com as informações do usuário
+        UserProfileDTO profile = new UserProfileDTO(
+            user.getName(),
+            user.getLevel(),
+            user.getXp(),
+            user.getProfilePicture()
+        );
+
+        return ResponseEntity.ok(profile);
     }
 }
--- src/main/java/com/jpeccia/levelinglife/dto/UserProfileDTO.java
+++ src/main/java/com/jpeccia/levelinglife/dto/UserProfileDTO.java
@@ -1,5 +1,14 @@
 package com.jpeccia.levelinglife.dto;
 
-public record UserProfileDTO(String name, String username, int level, int xp, String profilePicture) {
-    
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileDTO {
+    private String name;
+    private int level;
+    private int xp;
+    private String profilePicture;
 }
