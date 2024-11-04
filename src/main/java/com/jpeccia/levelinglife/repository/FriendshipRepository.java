00ff748c7a39329baf696ac32b28a27c2cbb7cf2
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-04 18:45:34 -0300

    refactor: friiendship logic

--- src/main/java/com/jpeccia/levelinglife/repository/FriendshipRepository.java
+++ src/main/java/com/jpeccia/levelinglife/repository/FriendshipRepository.java
@@ -1,17 +1,13 @@
 package com.jpeccia.levelinglife.repository;
 
 import com.jpeccia.levelinglife.entity.Friendship;
-import com.jpeccia.levelinglife.entity.User;
+import com.jpeccia.levelinglife.entity.FriendshipStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
-    // Retorna uma lista de amigos de um usuário
-    List<Friendship> findByUser(User user);
+    List<Friendship> findAllByRequesterIdAndStatusOrFriendIdAndStatus(Long requesterId, FriendshipStatus status, Long friendId, FriendshipStatus friendStatus);
 
-    // Verifica se uma amizade já existe entre dois usuários
-    Optional<Friendship> findByUserAndFriend(User user, User friend);
 }
