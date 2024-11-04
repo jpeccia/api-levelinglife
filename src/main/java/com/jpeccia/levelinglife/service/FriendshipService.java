831f3acf636b78d44a7c912e9086531aed553651
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-04 18:22:58 -0300

    feat: create Friendship Servicie

---
+++ src/main/java/com/jpeccia/levelinglife/service/FriendshipService.java
@@ -0,0 +1,44 @@
+package com.jpeccia.levelinglife.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jpeccia.levelinglife.entity.Friendship;
+import com.jpeccia.levelinglife.entity.User;
+import com.jpeccia.levelinglife.repository.FriendshipRepository;
+import com.jpeccia.levelinglife.repository.UserRepository;
+
+@Service
+public class FriendshipService {
+    
+    @Autowired
+    private FriendshipRepository friendshipRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public String addFriend(User user, Long friendId){
+        User friend = userRepository.findById(friendId).orElseThrow(() -> new RuntimeException("Friend not found"));
+
+        if(friendshipRepository.findByUserAndFriend(user, friend).isPresent()){
+            return "Você já é amigo desse usuário";
+        }
+
+        // Salva a amizade no banco de dados
+        friendshipRepository.save(new Friendship(user, friend));
+        friendshipRepository.save(new Friendship(friend, user)); // Amizade bidirecional
+
+        return "Amizade adicionada com sucesso.";
+    }
+
+    // Retorna a lista de amigos de um usuário
+    public List<User> getFriends(User user) {
+        List<Friendship> friendships = friendshipRepository.findByUser(user);
+        return friendships.stream()
+                .map(Friendship::getFriend)
+                .collect(Collectors.toList());
+    }
+}
