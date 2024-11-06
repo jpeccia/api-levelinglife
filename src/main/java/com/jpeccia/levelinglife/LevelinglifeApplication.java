faaf17c03c8ce78407542fab0e10bcf5d3be34f2
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-06 18:48:10 -0300

    build: updating Swagger

--- src/main/java/com/jpeccia/levelinglife/LevelinglifeApplication.java
+++ src/main/java/com/jpeccia/levelinglife/LevelinglifeApplication.java
@@ -3,21 +3,7 @@ package com.jpeccia.levelinglife;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
 @SpringBootApplication
-@OpenAPIDefinition(
-	info = @Info(
-		title = "LEVELING LIFE",
-		description = "API LevelingLife é uma plataforma que permite aos usuários criar e gerenciar seu perfil de evolução em um ambiente de gamificação, com foco no desenvolvimento de habilidades e no acompanhamento de progresso por meio de níveis e experiência (XP).",
-					   version = "1"
-	)
-)
-@SecurityScheme(name = "jwt_auth", scheme = "Bearer ", bearerFormat = "JWT", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class LevelinglifeApplication {
 
 	public static void main(String[] args) {
