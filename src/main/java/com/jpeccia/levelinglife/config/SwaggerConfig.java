e053f56f6df2db8d84cf03b5e05ce2c651018d71
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-06 18:28:59 -0300

    feat: configure swaggerconfig

--- src/main/java/com/jpeccia/levelinglife/config/SwaggerConfig.java
+++ src/main/java/com/jpeccia/levelinglife/config/SwaggerConfig.java
@@ -1,17 +1,27 @@
 package com.jpeccia.levelinglife.config;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("public")
-                .pathsToMatch("/**") // Aplica o Swagger a todos os endpoints
-                .build();
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+        .info(new Info().title("LevelingLife").description("API LevelingLife é uma plataforma que permite aos usuários criar e gerenciar seu perfil de evolução em um ambiente de gamificação, com foco no desenvolvimento de habilidades e no acompanhamento de progresso por meio de níveis e experiência (XP).").version("1"))
+        .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+        .components(new Components().addSecuritySchemes("Bearer Authentication", creaSecurityScheme()));
+    }
+
+    private SecurityScheme creaSecurityScheme(){
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("Bearer ").bearerFormat("JWT");
     }
 }
