313bbe929d067d186490ea183763837dae3bc588
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-06 18:12:19 -0300

    feat; create SwaggerConfig

---
+++ src/main/java/com/jpeccia/levelinglife/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.jpeccia.levelinglife.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**") // Aplica o Swagger a todos os endpoints
+                .build();
+    }
+}
