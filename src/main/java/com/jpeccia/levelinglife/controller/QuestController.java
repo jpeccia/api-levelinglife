ae839596e1afbb0410fa0a49702f50fe77c769c4
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-07 17:40:41 -0300

    feat: add quest edit in questController

--- src/main/java/com/jpeccia/levelinglife/controller/QuestController.java
+++ src/main/java/com/jpeccia/levelinglife/controller/QuestController.java
@@ -46,65 +46,108 @@ public class QuestController {
     TokenService tokenService;
 
     // Listar quests por ID de usuário
-    @Operation(summary = "Listar quests ativas do usuário", description = "Retorna todas as quests ativas de um usuário autenticado.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Quests retornadas com sucesso."),
-            @ApiResponse(responseCode = "401", description = "Token inválido ou usuário não autenticado.")
-    })
-    @GetMapping("/")
-    public ResponseEntity<List<Quest>> getUserQuests(@RequestHeader("Authorization") String authHeader) {
-        // Extrair o token JWT do cabeçalho e validá-lo
-        String token = authHeader.replace("Bearer ", "");
-        String username = tokenService.validateToken(token);
-        
-        if (username == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        @Operation(summary = "Listar quests ativas do usuário", description = "Retorna todas as quests ativas de um usuário autenticado.")
+        @ApiResponses({
+                @ApiResponse(responseCode = "200", description = "Quests retornadas com sucesso."),
+                @ApiResponse(responseCode = "401", description = "Token inválido ou usuário não autenticado.")
+        })
+        @GetMapping("/")
+        public ResponseEntity<List<Quest>> getUserQuests(@RequestHeader("Authorization") String authHeader) {
+            // Extrair o token JWT do cabeçalho e validá-lo
+            String token = authHeader.replace("Bearer ", "");
+            String username = tokenService.validateToken(token);
+            
+            if (username == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            // Buscar o usuário no banco de dados
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new RuntimeException("User not found!"));
+
+
+            // Recuperar todas as quests associadas ao usuário
+            List<Quest> activeQuests = repository.findByUserIdAndCompletedAtIsNull(user.getId());
+            return ResponseEntity.ok(activeQuests);
         }
 
-        // Buscar o usuário no banco de dados
-        User user = userRepository.findByEmail(username)
+        // Adicionar uma nova quest
+        @Operation(summary = "Adicionar uma nova quest", description = "Adiciona uma nova quest para o usuário autenticado.")
+        @ApiResponses({
+                @ApiResponse(responseCode = "200", description = "Quest adicionada com sucesso."),
+                @ApiResponse(responseCode = "401", description = "Token inválido ou usuário não autenticado."),
+                @ApiResponse(responseCode = "400", description = "Dados inválidos para criação da quest.")
+        })
+        @PostMapping("/")
+        public ResponseEntity<Quest> addQuest(@RequestBody QuestDTO body, HttpServletRequest request) {
+            String token = request.getHeader("Authorization").replace("Bearer ", "");
+            System.out.println("Received token for validation: " + token);
+        
+            String username = tokenService.validateToken(token);
+            if (username == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            }
+            User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new RuntimeException("User not found!"));
 
-
-        // Recuperar todas as quests associadas ao usuário
-        List<Quest> activeQuests = repository.findByUserIdAndCompletedAtIsNull(user.getId());
-        return ResponseEntity.ok(activeQuests);
-    }
-
-    // Adicionar uma nova quest
-    @Operation(summary = "Adicionar uma nova quest", description = "Adiciona uma nova quest para o usuário autenticado.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Quest adicionada com sucesso."),
-            @ApiResponse(responseCode = "401", description = "Token inválido ou usuário não autenticado."),
-            @ApiResponse(responseCode = "400", description = "Dados inválidos para criação da quest.")
-    })
-    @PostMapping("/")
-    public ResponseEntity<Quest> addQuest(@RequestBody QuestDTO body, HttpServletRequest request) {
-        String token = request.getHeader("Authorization").replace("Bearer ", "");
-        System.out.println("Received token for validation: " + token);
-    
-        String username = tokenService.validateToken(token);
-        if (username == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            Quest newQuest = questService.createQuest(body, user.getId());
+            return ResponseEntity.ok(newQuest);
         }
-        User user = userRepository.findByEmail(username)
-            .orElseThrow(() -> new RuntimeException("User not found!"));
 
-        Quest newQuest = questService.createQuest(body, user.getId());
-        return ResponseEntity.ok(newQuest);
-    }
+        // Atualizar uma quest existente
+        @Operation(summary = "Atualizar uma quest", description = "Atualiza uma quest existente do usuário autenticado.")
+        @ApiResponses({
+                @ApiResponse(responseCode = "200", description = "Quest atualizada com sucesso."),
+                @ApiResponse(responseCode = "404", description = "Quest não encontrada ou usuário não autorizado."),
+                @ApiResponse(responseCode = "401", description = "Token inválido ou usuário não autenticado.")
+        })
+        @PutMapping("/{id}")
+        public ResponseEntity<Quest> updateQuest(
+                @PathVariable Long id,
+                @RequestBody QuestDTO questDTO,
+                HttpServletRequest request) {
 
-    // Marcar uma quest como completa
-    @Operation(summary = "Marcar uma quest como completa", description = "Marca uma quest como completada.")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Quest marcada como completada."),
-            @ApiResponse(responseCode = "404", description = "Quest não encontrada.")
-    })
-    @PutMapping("/{id}/complete")
-    public ResponseEntity<Void> completeQuest(@PathVariable Long id){
-        questService.completeQuest(id);
-        return ResponseEntity.ok().build();
-    }
+            // Extrair e validar o token JWT
+            String token = request.getHeader("Authorization").replace("Bearer ", "");
+            String username = tokenService.validateToken(token);
+            if (username == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            // Buscar o usuário autenticado
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new RuntimeException("User not found!"));
+
+            // Buscar a quest pelo ID e garantir que pertence ao usuário autenticado
+            Quest quest = repository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Quest not found!"));
+
+            if (!quest.getUser().getId().equals(user.getId())) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+
+            // Atualizar os detalhes da quest com os dados do DTO
+            quest.setTitle(questDTO.getTitle());
+            quest.setDescription(questDTO.getDescription());
+            quest.setType(questDTO.getType());
+
+            // Salvar as alterações
+            repository.save(quest);
+
+            return ResponseEntity.ok(quest);
+        }
+
+        // Marcar uma quest como completa
+        @Operation(summary = "Marcar uma quest como completa", description = "Marca uma quest como completada.")
+        @ApiResponses({
+                @ApiResponse(responseCode = "200", description = "Quest marcada como completada."),
+                @ApiResponse(responseCode = "404", description = "Quest não encontrada.")
+        })
+        @PutMapping("/{id}/complete")
+        public ResponseEntity<Void> completeQuest(@PathVariable Long id){
+            questService.completeQuest(id);
+            return ResponseEntity.ok().build();
+        }
 
     // Deletar uma quest por ID
     @Operation(summary = "Deletar uma quest", description = "Deleta uma quest existente.")
