d0d7e6ca8726e6254b7351e7b86caba3919333b2
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-21 23:57:03 -0300

    feat: add in ChatMessageRepository methods

--- src/main/java/com/jpeccia/levelinglife/repository/ChatMessageRepository.java
+++ src/main/java/com/jpeccia/levelinglife/repository/ChatMessageRepository.java
@@ -1,9 +1,12 @@
 package com.jpeccia.levelinglife.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jpeccia.levelinglife.entity.ChatMessage;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    // Adicione métodos customizados, se necessário
+    List<ChatMessage> findBySenderAndRecipient(String sender, String recipient);
+    List<ChatMessage> findByRecipientAndSender(String recipient, String sender);
 }
