2313e2f091465116799d6fd11f4d7710a94eae46
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2024-11-27 19:49:44 -0300

    feat: remove achievements again

--- src/main/java/com/jpeccia/levelinglife/entity/User.java
+++ src/main/java/com/jpeccia/levelinglife/entity/User.java
@@ -55,8 +55,4 @@ public class User {
     
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Note> notes;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Achievement> achievements;
-
 }
